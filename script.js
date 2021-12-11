0a4e5aa6e72115489eaf3926980cbc799195e0e5
Dominic Gerth <d.gerth1990@googlemail.com>
2021-12-11 15:15:18 +0100

    made a function that checks for winner

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let fields = [];
-let currentShape = 'cross'
+let currentShape = 'cross';
+let winner;
 
 function fillShape(id) {
     fields[id] = currentShape;
@@ -10,6 +11,7 @@ function fillShape(id) {
         currentShape = 'cross';
     };
     draw();
+    checkForWin();
 }
 
 function draw(){
@@ -21,4 +23,38 @@ function draw(){
             document.getElementById('circle-' +i).classList.remove('d-none');
         };
     }
+}
+
+function checkForWin(){
+    if(fields[0] == fields[1] && fields[1] == fields[2] && fields[0]){
+        winner = fields[0];
+    }
+    else if(fields[3] == fields[4] && fields[4] == fields[5] && fields[3]){
+        winner = fields[3];
+    }
+    else if(fields[6] == fields[7] && fields[7] == fields[8] && fields[6]){
+        winner = fields[6];
+    }
+    else if(fields[6] == fields[4] && fields[4] == fields[2] && fields[6]){
+        winner = fields[6];
+    }
+    else if(fields[0] == fields[4] && fields[4] == fields[8] && fields[0]){
+        winner = fields[0];
+    }
+    else if(fields[0] == fields[3] && fields[3] == fields[6] && fields[0]){
+        winner = fields[0];
+    }
+    else if(fields[1] == fields[4] && fields[4] == fields[7] && fields[1]){
+        winner = fields[1];
+    }
+    else if(fields[2] == fields[5] && fields[5] == fields[8] && fields[2]){
+        winner = fields[2];
+    }
+    if (winner){
+    printOutWinner();
+    }
+}
+
+function printOutWinner(){
+console.log(winner+' hat gewonnen');
 }
